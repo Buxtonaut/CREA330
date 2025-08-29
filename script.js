7bc07594caec6a5b45addbe8addcd1fbd9cbe079
Buxtonaut <mcelhoneyrj@vcu.edu>
2025-08-28 21:21:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const keys = document.querySelectorAll('.key');
 
+//Listener for all keys that runs function on click action
 keys.forEach((key) => {
   key.addEventListener('click', () => playNote(key));
 });
 
+//playNote function definition
 function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note);
   noteAudio.currentTime = 0;
   noteAudio.play();
   key.classList.add('active');
+//Add active style class to clicked elements
+    key.classList.remove('active');
+//Remove active class
+    noteAudio.addEventListener('ended', () => {
+  });
 }
 
-noteAudio.addEventListener('ended', () => {
-    key.classList.remove('active');
-  });
 
 const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
@@ -27,6 +31,11 @@ document.addEventListener('keydown', (e) => {
   const whiteKeyIndex = WHITE_KEYS.indexOf(key);
   const blackKeyIndex = BLACK_KEYS.indexOf(key);
 
+   // If the key is being held down, don't play the note again
+  if (e.repeat) {
+    return;
+  }
+
   if (whiteKeyIndex > -1) {
     playNote(whiteKeys[whiteKeyIndex]);
   }
@@ -34,9 +43,11 @@ document.addEventListener('keydown', (e) => {
   if (blackKeyIndex > -1) {
     playNote(blackKeys[blackKeyIndex]);
   }
+
+ 
 });
 
-// If the key is being held down, don't play the note again
-  if (e.repeat) {
-    return;
-  }
+
+//TTLS Notes - C C G G A A G, F F E E D D C, G G F F E E D, G G F F E E D, C C G G A A G, F F E E D D C
+
+//TTLS 
